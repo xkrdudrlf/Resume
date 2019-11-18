b13bd2cc9a1c611c724449051f7e0980589b001c
Tom Tak <y0unggil0919@gmail.com>
2019-11-18 17:36:05 +1100

    reset dungeonExplorer

--- Projects/DungeonExplorer/src/unsw/dungeon/Enum.java
+++
@@ -1,9 +0,0 @@
-package unsw.dungeon;
-/**
- * Enum class has 4 possible values for direction(NORTH/SOUTH/WEST/EAST).
- */
-public final class Enum {
-	public static enum Direction {
-		NORTH,SOUTH,WEST,EAST
-	}
-}
